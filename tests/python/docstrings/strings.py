5992bc02b0a29e393708c19f1c22f866389756d0
Erik Schierboom <erik_schierboom@hotmail.com>
2023-04-06 12:50:57 +0200

    Don't count docstrings as code (#108)

---
+++ tests/python/docstrings/strings.py
@@ -0,0 +1,14 @@
+"""Functions for creating, transforming, and adding prefixes to strings.
+
+And many more functions too
+"""
+
+
+def add_prefix_un(word):
+    """Take the given word and add the 'un' prefix.
+
+    :param word: str - containing the root word.
+    :return: str - of root word prepended with 'un'.
+    """
+
+    return 'un' + word
